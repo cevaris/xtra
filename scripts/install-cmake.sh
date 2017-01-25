ffd2df84d24239044277a52f2b41222eabe65acc
Adam Cardenas <cevaris@gmail.com>
2017-01-24 20:17:10 -0700

    removed travis_retry command

--- scripts/install-cmake.sh
+++ scripts/install-cmake.sh
@@ -2,7 +2,7 @@
 
 if [[ "${TRAVIS_OS_NAME}" == "linux" ]]; then
     CMAKE_URL="https://cmake.org/files/v3.6/cmake-3.6.3-Linux-x86_64.tar.gz"
-    mkdir cmake && travis_retry wget --quiet -O - ${CMAKE_URL} | tar --strip-components=1 -xz -C cmake
+    mkdir cmake && wget --quiet -O - ${CMAKE_URL} | tar --strip-components=1 -xz -C cmake
     export PATH=${DEPS_DIR}/cmake/bin:${PATH}
 else
     brew install cmake
