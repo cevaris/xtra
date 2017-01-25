596948378b8f2cea4cd224b4401399c4e73ff232
Adam Cardenas <cevaris@gmail.com>
2017-01-24 20:23:27 -0700

    manual download of cmake bins

--- scripts/install-cmake.sh
+++ scripts/install-cmake.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env bash
 
-if [[ "${TRAVIS_OS_NAME}" == "linux" ]]; then
+#if [[ "${TRAVIS_OS_NAME}" == "linux" ]]; then
     CMAKE_URL="https://cmake.org/files/v3.6/cmake-3.6.3-Linux-x86_64.tar.gz"
-    mkdir cmake && wget --quiet -O - ${CMAKE_URL} | tar --strip-components=1 -xz -C cmake
-    export PATH=${DEPS_DIR}/cmake/bin:${PATH}
-else
-    brew install cmake
-fi
+    wget ${CMAKE_URL}
+    tar xf cmake-3.6.3-Linux-x86_64.tar.gz
+    export PATH=${DEPS_DIR}/cmake-3.6.3-Linux-x86_64/bin:${PATH}
+#else
+#    brew install cmake
+#fi
